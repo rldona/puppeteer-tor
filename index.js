a7984f3c87ef4952ccc12973e58ecbf23e03e523
Raúl López <rldona@gmail.com>
2020-12-29 19:29:57 +0100

    added new feature

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 
 async function main() {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
