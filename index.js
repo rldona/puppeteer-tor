94b98e4c107dd4ed77a1d6f022fd984d441bfdff
Raúl López <rldona@gmail.com>
2020-12-29 20:15:40 +0100

    added new feature

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function main() {
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--proxy-server=socks5://127.0.0.1:9052'
+      '--proxy-server=socks5://127.0.0.1:9054'
     ]
   });
 
