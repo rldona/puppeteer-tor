7be65f68a68564d588a79125d46444d9b9cd6199
Raúl López <rldona@gmail.com>
2021-01-01 23:15:57 +0100

    added new feature

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const puppeteer = require('puppeteer');
 
-async function main(randomNumber) {
+let counter = 0;
+
+async function main(id, randomNumber) {
   const browser = await puppeteer.launch({
     headless: true,
     ignoreHTTPSErrors: true,
@@ -12,31 +14,48 @@ async function main(randomNumber) {
     ]
   });
 
+  ////
+
   const page = await browser.newPage();
 
   await page.setRequestInterception(true);
 
   page.on('request', (request) => {
-      if (request.resourceType() === 'document') {
-          request.continue();
-      } else {
-          request.abort();
-      }
+    if (request.resourceType() === 'document') {
+        request.continue();
+    } else {
+        request.abort();
+    }
   });
 
-  await page.goto('https://whatismycountry.com', { timeout: 0 });
+  ////
 
-  const h2Tags = await page.$$eval('h2', h2 => (h2[0].textContent).split('Your Country is ')[1]);
+  const url = `https://www.filmaffinity.com/es/film${id}.html`;
 
-  console.log(h2Tags);
+  let browserLoad = await page.goto(url, { waitUntil: 'load', timeout: 0 });
+
+  if (browserLoad.status() === 200) {
+    const title = await page.evaluate(() => {
+      return document.querySelector('[itemprop="name"]') ? document.querySelector('[itemprop="name"]').textContent : '';
+    });
+    console.log(`==> ${id} | ${title} <==`);
+    counter++;
+  }
+
+  ////
 
   await browser.close();
 
 }
 
 (async () => {
-  for (let id = 0; id < 10 ; id++) {
-    await main(Math.floor(Math.random() * (82 - 52 + 1)) + 52);
+
+  for (let id = 100000; id < 200000 ; id++) {
+    await main(id, Math.floor(Math.random() * (99 - 52 + 1)) + 52);
   }
+
+  console.log(`\n\n==> Scrappig finished ==> ${counter}/100000 reviews in total <==\n\n`);
+
   process.exit(1);
+
 })();
