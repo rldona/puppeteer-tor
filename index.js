4532341d25f0168964eae6bd1e6f9e7a25171ef7
Raúl López <rldona@gmail.com>
2021-01-01 19:12:18 +0100

    added new feature

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 
 const puppeteer = require('puppeteer');
-const ipify = require('ipify');
+const getProxies = require("get-free-https-proxy");
 
-async function main() {
+async function main(proxy) {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
-      '--proxy-server=socks5://127.0.0.1:9054'
+      // `--proxy-server=${proxy.host}:${proxy.port}`
     ]
   });
 
   const page = await browser.newPage();
 
-  await page.goto('https://api.ipify.org');
+  await page.goto('https://whatismycountry.com/');
 
-  const ip = await ipify({ useIPv6: false });
+  const result = await page.evaluate(async () => {
+    return document.querySelector('h2').textContent;
+  });
 
-  console.log(ip);
+  console.log(result);
 
-  browser.close();
+  await browser.close();
+
 }
 
 //// => 139.47.114.63 (Sabadell)
 //// => 134.209.226.5 (DigitalOcean)
-//// => xxx.xxx.xxx.x (Tor)
+//// => 185.124.31.83 (Proxy List)
 
-main();
+(async () => {
+
+  // const proxies = await getProxies();
+  // const randomProxy = proxies[Math.floor(Math.random() * (proxies.length - 0 + 1)) + 0];
+
+  const randomProxy = {
+    host: '198.50.163.192',
+    port: '3129'
+  }
+
+  // for (let id = 0; id < 100000; id++) {
+  await main(randomProxy);
+  // }
+
+})();
