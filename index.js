7eaff27c13ad99f2641472ac2f2b34fd8b44b634
Raúl López <rldona@gmail.com>
2021-01-01 22:12:04 +0100

    added new feature

--- index.js
+++ index.js
@@ -14,21 +14,12 @@ async function main(randomNumber) {
 
   const page = await browser.newPage();
 
-  // await page.setRequestInterception(true);
+  await page.goto('https://ipinfo.io/json', { timeout: 0 });
 
-  // page.on('request', (request) => {
-  //     if (request.resourceType() === 'document') {
-  //         request.continue();
-  //     } else {
-  //         request.abort();
-  //     }
-  // });
+  const content = await page.content();
+  const serialized = content.substring(content.indexOf('{'), content.indexOf('}') + 1);
 
-  await page.goto('https://www.filmaffinity.com/es/film335397.html', { timeout: 0 });
-
-  const h2Tags = await page.$$eval('h2', h2 => h2.length);
-
-  console.log(h2Tags);
+  console.log(JSON.parse(serialized));
 
   await browser.close();
 
@@ -39,14 +30,12 @@ async function main(randomNumber) {
   let randomNumber;
 
   for (let id = 0; id < 1000 ; id++) {
-    if (id !== 0 && id % 10 === 0) {
-      console.log(`\n\n[========================================]`);
-      console.log(`[==> ${id} <==]    Ramdonized    [==> ${id} <==]`);
-      console.log(`[========================================]\n`);
-      randomNumber = Math.floor(Math.random() * (62 - 52 + 1)) + 52;
+    if (id !== 0 && id % 500 === 0) {
+      console.log(`[: ${id} :] Ramdonized [: ${id} :]`);
+      randomNumber = Math.floor(Math.random() * (72 - 52 + 1)) + 52;
     }
 
-    console.log(`\n[==> ${id} <==] Scrapping [==> ${id} <==]\n`);
+    console.log(`[==> ${id} <==] Scrapping [==> ${id} <==]`);
 
     await main(randomNumber);
   }
